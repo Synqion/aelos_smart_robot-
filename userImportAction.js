4f01ff34dacd396718e06b5e4b59685c460b5232
Deadnumb <14127859+deadnumb@user.noreply.gitee.com>
2026-06-27 10:29:45 +0800

    添加了更激进的代码

--- userImportAction.js
+++ userImportAction.js
@@ -936,3 +936,51 @@ Blockly.Python['1761534066416'] = function(block) {
   return code;
 }
 
+Blockly.Blocks['1782522901042'] = {
+  init: function() {
+    this.jsonInit({
+      "type": "1782522901042",
+      "message0": "GrabCubeCustom",
+      "previousStatement": "motion_block",
+      "nextStatement": "motion_block",
+      "colour": '#C643F1',
+      "toolip": "",
+      "helpUrl": ""
+    });
+  }
+};
+
+Blockly.Lua['1782522901042'] = function(block) {
+  let code = "MOTOsetspeed(30)\nMOTOrigid16(40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,0,0,0)\n\n\n-- 蹲\nMOTOsetspeed(14)\nMOTOmove19(80, 30, 100, 100, 130, 150, 50, 100, 120, 170, 100, 100, 70, 50, 150, 100, 0, 0, 100)\nMOTOwait()\nDelayMs(500)\n\n\n-- 伸手\nMOTOsetspeed(14)\nMOTOmove19(80, 30, 150, 100, 130, 150, 50, 100, 120, 170, 50, 100, 70, 50, 150, 100, 0, 0, 100)\nMOTOwait()\nDelayMs(500)\n\n\n-- 拿捏\nMOTOsetspeed(14)\nMOTOmove19(60, 10, 150, 100, 130, 150, 50, 100, 140, 190, 50, 100, 70, 50, 150, 100, 0, 0, 100)\nMOTOwait()\nDelayMs(500)\n\n\n-- 举起\nMOTOsetspeed(14)\nMOTOmove19(60, 10, 190, 100, 93, 55, 124, 100, 140, 190, 10, 100, 107, 145, 76, 100, 0, 0, 100)\nMOTOwait()\nDelayMs(500)\n";
+  return code;
+}
+
+Blockly.Python['1782522901042'] = function(block) {
+  let code = "base_action.action('GrabCubeCustom')\n";
+  return code;
+}
+
+Blockly.Blocks['1782523833886'] = {
+  init: function() {
+    this.jsonInit({
+      "type": "1782523833886",
+      "message0": "GrabCubeV2（2）",
+      "previousStatement": "motion_block",
+      "nextStatement": "motion_block",
+      "colour": '#C643F1',
+      "toolip": "",
+      "helpUrl": ""
+    });
+  }
+};
+
+Blockly.Lua['1782523833886'] = function(block) {
+  let code = "MOTOsetspeed(30)\nMOTOrigid16(40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,0,0,0)\nMOTOsetspeed(30)\nMOTOmove19(80, 30, 100, 100, 93, 55, 124, 100, 120, 170, 100, 100, 107, 145, 76, 100, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(10)\nMOTOmove19(80, 56, 115, 65, 127, 150, 51, 66, 118, 141, 101, 142, 81, 50, 153, 139, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(10)\nMOTOmove19(88, 73, 116, 57, 142, 150, 48, 66, 119, 126, 83, 143, 60, 50, 151, 140, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(10)\nMOTOmove19(68, 49, 165, 47, 154, 142, 47, 62, 120, 160, 39, 156, 45, 57, 153, 139, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(30)\nMOTOmove19(60, 17, 163, 47, 154, 142, 47, 62, 136, 186, 40, 156, 45, 57, 153, 139, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(10)\nMOTOmove19(60, 17, 163, 57, 140, 150, 69, 62, 136, 186, 40, 138, 85, 50, 152, 139, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(10)\nMOTOmove19(60, 17, 163, 95, 105, 150, 39, 99, 136, 186, 40, 98, 99, 50, 163, 97, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(10)\nMOTOmove19(60, 17, 163, 97, 96, 150, 39, 99, 136, 186, 40, 99, 107, 50, 163, 98, 0, 0, 100)\nMOTOwait()\nMOTOsetspeed(10)\nMOTOmove19(60, 17, 163, 98, 102, 77, 115, 100, 136, 186, 40, 99, 99, 123, 86, 98, 0, 0, 100)\nMOTOwait()\n";
+  return code;
+}
+
+Blockly.Python['1782523833886'] = function(block) {
+  let code = "base_action.action('GrabCubeV2（2）')\n";
+  return code;
+}
+
